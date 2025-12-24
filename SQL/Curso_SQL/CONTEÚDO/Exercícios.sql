80061b2bfbda6057d29d8a0fd72ab8057363b08a
jacksoncroque <jackson.crq@gmail.com>
2025-12-24 15:55:33 -0300

    Prática de aprendizagem

---
+++ SQL/Curso_SQL/CONTEÚDO/Exercícios.sql
@@ -0,0 +1,47 @@
+--LISTE TODAS AS TRANSAÇÕES DE APENAS 1 PONTO
+SELECT * 
+FROM transacoes
+WHERE qtdePontos = 1;
+
+--LISTE TODOS OS PEDIDOS REALIZADOS NO FIM DE SEMANA
+SELECT * , Strftime ('%w', Datetime(Substr(DtCriacao, 1, 10))) AS DiasDaSemana
+FROM transacoes
+WHERE Strftime ('%w', Datetime(Substr(DtCriacao, 1, 10))) IN ('0', '6');
+
+--LISTE TODOS OS CLIENTES COM ZERO PONTOS
+SELECT idCliente, qtdePontos
+FROM clientes
+WHERE QtdePontos = 0;
+
+--LISTE TODOS OS CLIENTES COM 100 A 200 PONTOS (INCLUSIVE AMBOS)
+SELECT *
+FROM clientes
+WHERE qtdePontos>=100 AND qtdePontos<=200;
+
+--LISTE TODOS OS REGISTROS QUE COMEÇAM COM 'VENDA DE'
+SELECT * 
+FROM produtos
+WHERE DescNomeProduto LIKE 'Venda de%';
+
+--LISTE TODOS OS PRODUTOS COM NOME TERMINADO EM 'LOVER'
+SELECT *
+FROM produtos
+Where DescNomeProduto LIKE '%Lover';
+
+--LISTE TODOS OS PRODUTOS QUE SÃO UM CHAPÉU
+SELECT * 
+FROM produtos
+WHERE DescNomeProduto LIKE '%Chapéu%';
+
+--LISTE TODAS AS TRANSAÇÕES COM O PRODUTO RESGATAR PONEI
+SELECT *
+FROM produtos
+WHERE DescNomeProduto = 'Resgatar Ponei';
+
+/* LISTAR TODAS AS TRANSAÇÕES ADICIONANDO UMA COLUNA NOVA SINALIZANDO “ALTO”, “MÉDIO” E “BAIXO” PARA O
+ VALOR DOS PONTOS (<10; <500; >=500) */
+ SELECT *, QtdePontos>=500 AS 'ALTO',
+        QtdePontos<10 AS 'BAIXO',
+        QtdePontos<500 AND QtdePontos>10 AS 'MÉDIO'
+ FROM transacoes
+ 
