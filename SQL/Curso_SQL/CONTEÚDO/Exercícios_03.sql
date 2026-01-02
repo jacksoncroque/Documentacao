1654874d86aeb18021131fb01b08258bd64c16ec
jacksoncroque <jackson.crq@gmail.com>
2026-01-02 17:08:35 -0300

    Progresso do aprendizado

--- SQL/Curso_SQL/CONTEÚDO/Exercícios_03.sql
+++ SQL/Curso_SQL/CONTEÚDO/Exercícios_03.sql
@@ -1,4 +1,4 @@
--- Qual categoria de produti foi mais vendida?
+-- QUAL CATEGORIA DE PRODUTO FOI MAIS VENDIDA?
 
 SELECT t1.idTransacaoProduto,
        t2.DescCategoriaProduto,
@@ -49,4 +49,70 @@ WHERE DescNomeProduto = 'Lista de presença'
 GROUP BY substr(DtCriacao,1,7) 
 ORDER BY count(DISTINCT t1.IdTransacao) DESC
 
-LIMIT 100
+LIMIT 100;
+
+-- QUAIS CLIENTES PERDERAM PONTOS POR 'LOVER'?
+
+SELECT t1.idCliente,
+       --t1.IdTransacao,
+       --t2.IdProduto,
+       --t3.DescCategoriaProduto,
+       --t3.DescNomeProduto,
+       --t1.qtdePontos,
+       sum(t1.qtdePontos) AS qntdPontos
+
+FROM transacoes AS t1
+
+LEFT JOIN transacao_produto AS t2
+ON t1.IdTransacao = t2.IdTransacao
+
+LEFT JOIN produtos AS t3
+ON t2.IdProduto = t3.IdProduto
+
+WHERE DescCategoriaProduto = 'lovers'
+
+GROUP BY t1.idCliente
+
+ORDER BY sum(t1.qtdePontos) ASC
+
+LIMIT 10;
+
+-- QUAIS CLIENTES ASSINARAM A LISTA DE PRESENÇA NO DIA 2025/08/25?
+
+SELECT t1.idCliente,
+       substr(t1.DtCriacao, 1, 10) AS dtCriacao,
+       t3.DescNomeProduto
+FROM transacoes AS t1
+
+LEFT JOIN transacao_produto AS t2
+ON t1.IdTransacao = t2.IdTransacao
+
+LEFT JOIN produtos AS t3
+ON t2.IdProduto = t3.IdProduto
+
+WHERE substr(t1.DtCriacao, 1, 10) = '2025-08-25'
+AND DescNomeProduto = 'Lista de presença'
+
+GROUP BY t1.idCliente;
+
+-- Do início ao fim do nosso curso (2025/08/25 a 2025/08/29), quantos clientes
+-- assinaram a lista de presença?
+
+SELECT t1.idCliente,
+       substr(t1.DtCriacao, 1, 10) AS dtCriacao,
+       t3.DescNomeProduto,
+       count(DISTINCT t1.idCliente)
+
+FROM transacoes AS t1
+
+LEFT JOIN transacao_produto AS t2
+ON t1.IdTransacao = t2.IdTransacao
+
+LEFT JOIN produtos AS t3
+ON t2.IdProduto = t3.IdProduto
+
+WHERE DtCriacao >= '2025-08-25'
+AND DtCriacao < '2025-08-29'
+AND DescNomeProduto = 'Lista de presença'
+
+GROUP BY t1.idCliente;
